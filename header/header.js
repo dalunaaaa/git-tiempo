59ac6b72577879e60a602c63eb8b9175047f3d19
danialuna <dmluna@scl.edu.gt>
2024-05-29 11:40:55 -0600

    actualizacion del header

--- header/header.js
+++ header/header.js
@@ -1,4 +1,5 @@
 let gl_header=document.querySelector(".header")
 gl_header.innerHTML=`
-<h1>🐋comandos git🐋</h1>
+<h1 class="titulo">comandos <span>Git</span></h1> 
+<a href="https://github.com/dalunaaaa/git-tiempo" class="btn-github">github</a>
 `;
